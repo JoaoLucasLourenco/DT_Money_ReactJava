3c7aa7ac79b78629300fdcbef4e3c45de06ecc75
João Lucas Lourenço <jao.lucas.lourenco@gmail.com>
2024-11-12 13:02:00 -0300

    Formatando o valores da info list

--- frontend/src/app/components/info-list/info-list.component.ts
+++ frontend/src/app/components/info-list/info-list.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { CurrencyPipe } from '@angular/common';
+import { Component, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
 @Component({
   selector: 'app-info-list',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './info-list.component.html',
-  styleUrl: './info-list.component.scss'
+  styleUrl: './info-list.component.scss',
+  providers: [
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ]
 })
 export class InfoListComponent {
-
+  entradas:number = 17400;
+  saidas:number = 1259;
+  total:number = this.entradas - this.saidas;
 }
--- frontend/src/app/model/Infos.ts
+++
@@ -1,6 +0,0 @@
-export class Info{
-    descricao:string;
-    valor:number;
-    total:boolean;
-    corIcon:string;
-}
